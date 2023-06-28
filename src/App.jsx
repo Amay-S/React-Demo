893a9b66ef9ba7400fe914500566505f71187402
Amay <amay.shah10@gmail.com>
2023-06-28 17:02:50 -0400

    commit
    - resume: 1:00:02
    - nav bar created
    - hero.jsx and computers.jsx in progress
    
    looks good so far

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { About, Contact, Experience, Feedbacks, Hero, Navbar,
 const App = () => {
   return (
     <BrowserRouter>
-      <div classnName="relative z-0 bg-primary">
+      <div className="relative z-0 bg-primary">
         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center"> 
           <Navbar />
           <Hero />
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <section className="relative w-full h-screen mx-auto">
+      <div
+        className={`${styles.paddingX} 
+        absolute insent-0 top-[120px] max-w-7xl 
+        mx-auto flex flex-row items-start gap-5`}
+      >
+        <div className="flex flex-col justify-center items-center mt-5">
+          <div className="w-5 h-5 rounded-full bg-[#915eff]" />
+          <div className="w-1 sm:h-80 h-40 violet-gradient" />
+        </div>
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>
+            Hello, my name is <span className="text-[#915eff]">Amay</span>
+          </h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+            I am a skilled AEM Developer with 4+ years of experience in
+            designing, developing, and implementing web-based applications using
+            Adobe Experience Manager (AEM 6.1, 6.4, 6.5) and Java EE.
+            <br className="sm:block hidden" /> I'm expected to graduate from
+            Toronto Metropolitan University in spring 2024, with a Bachelor of
+            Science Hons in Computer Science, and a minor in Information
+            Technology Management.
+          </p>
+        </div>
+      </div>
+      <ComputersCanvas />
+    </section>
+  );
+};
 
-export default Hero
+export default Hero;
